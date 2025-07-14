8f45af25b8eb60b4155b089a31ffc6aec7430689
JoVi-Yashi <saturnogx00@gmail.com>
2025-07-14 11:50:57 -0500

    FACTOR: cosas de DB

--- MATPI/DB/DB MATPI.sql
+++ MATPI/DB/DB MATPI.sql
@@ -3,36 +3,34 @@ use matpi;
 
 create table Usuario
 (
-ID varchar(16) primary key,
-Telefono varchar(14),
-Contraseña varchar(20),
-Correo_Electronico varchar(35),
+ID varchar(16) primary key not null,
+Telefono varchar(14) not null,
+Contraseña varchar(255) not null,
+Correo_Electronico varchar(35) not null,
 Rol enum(
-	'Activo', 
-	'Licencia', 
-	'Vacaciones', 
-	'Retirado', 
-	'Despedido'
-		),
-Fecha_Nacimiento date,
-Nombre_Completo varchar(40),
-Estado boolean,
-Direccion varchar(50),
-Fecha_ingreso date,
-Experiencia_Laboral varchar(15)
+	'Administrador', 
+	'Empleado'
+		) not null,
+Fecha_Nacimiento date null,
+Nombre_Completo varchar(40) not null,
+Estado boolean not null,
+Direccion varchar(50) not null,
+Fecha_ingreso date not null,
+Experiencia_Laboral varchar(15) not null,
+-- validaciones
+    constraint chk_email check (Correo_Electronico REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
+    constraint chk_telefono check (Telefono REGEXP '^[0-9+\-\s()]+$')
 );
 
 create table Administrador
 (
-Ult_Fecha_login datetime,
-Ult_IP_login varchar(45),
-Formacion_Educativa varchar(35),
-	ID_Usr varchar(16),
+Ult_Fecha_login datetime null,
+Ult_IP_login varchar(45) null,
+Formacion_Educativa varchar(35) null,
+	ID_Usr varchar(16) not null,
 		constraint Usuario_Hereda_Administrador 
 		foreign key (ID_Usr) references Usuario(ID)
 );
-
-
 
 create table Empleado
 (
@@ -55,62 +53,61 @@ EPS enum (
     'Asmet Salud', 
     'Emssanar', 
     'Capital Salud'
-			),
+			) not null,
 tipo_contrato enum(
 	'Indefinido',
 	'Fijo',
 	'Servicios',
 	'Temporal'
-					),
-Contacto_Emergencia_Nombre varchar(35), 
-Contacto_Emergencia_Parentesco varchar(15),
-Contacto_Emergencia_Numero varchar(14),
-Fecha_Terminacion_Contrato date,
-	ID_Usr varchar(16),
+					) not null,
+Contacto_Emergencia_Nombre varchar(35) not null, 
+Contacto_Emergencia_Parentesco varchar(15) not null,
+Contacto_Emergencia_Numero varchar(14) not null,
+Fecha_Terminacion_Contrato date null,
+	ID_Usr varchar(16) not null,
 		constraint Usuario_Hereda_Empleado 
 		foreign key (ID_Usr) references Usuario(ID) 
 );
 
 create table Cliente
 (
-ID smallint unsigned primary key,
-Nombre_Completo varchar(40),
-Telefono varchar(14),
-Ultima_Visita datetime,
-Total_Consumo int unsigned,
-	ID_Usr varchar(16),
+ID smallint unsigned primary key not null,
+Nombre_Completo varchar(40) not null,
+Telefono varchar(14) not null,
+Ultima_Visita datetime null,
+Total_Consumo int unsigned not null,
+Fecha_Registro timestamp not null,
+	ID_Usr varchar(16) not null,
 		constraint fk_empleado_cliente foreign key (ID_Usr) references Empleado(ID_Usr) 
 );
 
 create table Reserva
 (
-ID smallint unsigned primary key,
-Fecha datetime,
-Estado boolean,
-Observciones tinytext,
-	ID_Usr varchar(16),
+ID smallint unsigned primary key not null,
+Fecha datetime not null,
+Estado boolean not null,
+Observaciones tinytext null,
+	ID_Usr varchar(16) not null,
 		constraint fk_reserva_empleado foreign key (ID_Usr) references Empleado(ID_Usr)
 );
 
 create table Pedido
 (
-ID smallint unsigned primary key, 
-Fecha date,
-Estado boolean,
-Valor int unsigned,
-Mesa tinyint unsigned,
-	ID_Usr varchar(16),
-
+ID smallint unsigned primary key not null, 
+Fecha date not null,
+Estado boolean not null,
+Valor int unsigned not null,
+Mesa tinyint unsigned null,
+Numero_Personas tinyint unsigned null,
+	ID_Usr varchar(16) not null,
 		constraint fk_pedido_empleado 
 		foreign key (ID_Usr) 
 		references Empleado(ID_Usr),
-	ID_Reserva smallint unsigned,
+	ID_Reserva smallint unsigned null,
 		constraint fk_pedido_reserva 
 		foreign key (ID_Reserva) 
 		references Reserva(ID),
-
-
-	ID_Cliente smallint unsigned,
+	ID_Cliente smallint unsigned null,
 		constraint fk_pedido_cliente
         foreign key (ID_Cliente)
         references Cliente(ID)
@@ -120,22 +117,22 @@ Mesa tinyint unsigned,
 
 create table Proveedor
 (
-ID smallint unsigned primary key,
-Nombre_Proveedor varchar(50),
-Direccion varchar(120),
-Correo_Electronico varchar(35),
-Telefono varchar(14),
-	ID_Usr varchar(16),
+ID smallint unsigned primary key not null,
+Nombre_Proveedor varchar(50) not null,
+Direccion varchar(120) not null,
+Correo_Electronico varchar(35) null,
+Telefono varchar(14) not null,
+	ID_Usr varchar(16) not null,
 		constraint fk_proveedor_empleado foreign key (ID_Usr) references Empleado(ID_Usr)
 );
 
 create table Producto
 (
-ID smallint unsigned primary key,
-Nombre_Producto varchar(35),
-Descripcion tinytext,
-Cantidad smallint unsigned ,
-Valor int unsigned,
+ID smallint unsigned primary key not null,
+Nombre_Producto varchar(35) not null,
+Descripcion tinytext null,
+Cantidad smallint unsigned not null,
+Valor int unsigned not null,
 Categoria enum(
 	'Lácteos y Huevos',
     'Carnes y Aves',
@@ -151,17 +148,17 @@ Categoria enum(
     'Panadería', 
     'Envases y Empaques', 
     'Otros'
-			)
+			) not null
 );
 
 create table Factura
 (
-ID smallint unsigned primary key,
-Valor_Total int unsigned,
-Descripcion tinytext,
-IVA float,
-Metodo_Pago varchar(40),
-	ID_Pedi smallint unsigned,
+ID smallint unsigned primary key not null,
+Valor_Total int unsigned not null,
+Descripcion tinytext null,
+IVA float not null,
+Metodo_Pago varchar(40) not null,
+	ID_Pedi smallint unsigned not null,
 		constraint fk_factura_pedido
         foreign key (ID_Pedi)
         references Pedido(ID)
@@ -171,21 +168,21 @@ Metodo_Pago varchar(40),
 
 create table Materia_Prima
 (
-ID smallint unsigned primary key,
-Nombre_Materia_Prima varchar(60),
-Unidad_Medida varchar(20),
-Cantidad smallint unsigned,
-Fecha_Ingreso datetime,
-Fecha_Vencimiento date
+ID smallint unsigned primary key not null,
+Nombre_Materia_Prima varchar(60) not null,
+Unidad_Medida varchar(20) not null,
+Cantidad smallint unsigned not null,
+Fecha_Ingreso datetime not null,
+Fecha_Vencimiento date null
 );
 
 -- tablas intermedias
 
 create table Details_Producto_MateriaP
 (
- Producto_ID smallint unsigned,
-    MateriaPrima_ID smallint unsigned,
-    Cantidad_Usada smallint unsigned, 
+ Producto_ID smallint unsigned not null,
+    MateriaPrima_ID smallint unsigned not null,
+    Cantidad_Usada smallint unsigned not null, 
 
     primary key (Producto_ID, MateriaPrima_ID),
 
@@ -204,10 +201,10 @@ create table Details_Producto_MateriaP
 
 create table Details_Proveedor_MateriaP
 (
-    Proveedor_ID smallint unsigned,
-    MateriaPrima_ID smallint unsigned,
-    Precio_Unitario decimal(10,2),  
-    Fecha_Suministro datetime,   
+    Proveedor_ID smallint unsigned not null,
+    MateriaPrima_ID smallint unsigned not null,
+    Precio_Unitario decimal(10,2) not null,  
+    Fecha_Suministro datetime not null,   
 
     primary key (Proveedor_ID, MateriaPrima_ID),
 
@@ -221,15 +218,16 @@ create table Details_Proveedor_MateriaP
         foreign key (MateriaPrima_ID) 
         references Materia_Prima(ID)
         on delete cascade
-        on update cascade);
+        on update cascade
+);
 
 create table Details_Pedido_Producto
 (
-	ID smallint unsigned auto_increment primary key,
-    ID_Pedido smallint unsigned,
-    ID_Producto smallint unsigned,
-    Cantidad smallint unsigned,
-    Precio_Unitario int unsigned,
+	ID smallint unsigned auto_increment primary key not null,
+    ID_Pedido smallint unsigned not null,
+    ID_Producto smallint unsigned not null,
+    Cantidad smallint unsigned not null,
+    Precio_Unitario int unsigned not null,
 
     constraint fk_producto_pedido_pedido 
         foreign key (ID_Pedido) 
@@ -240,8 +238,15 @@ create table Details_Pedido_Producto
         foreign key (ID_Producto) 
         references Producto(ID)
         on delete cascade
-
 );
 
+--
+-- indices 
+--
+
+create index idx_usuario_correo on Usuario(Correo_Electronico);
+create index idx_pedido_fecha on Pedido(Fecha);
+create index idx_cliente_ultima_visita on Cliente(Ultima_Visita);
+create index idx_materia_prima_vencimiento on Materia_Prima(Fecha_Vencimiento);
 
 -- hash2 para contraseñas
